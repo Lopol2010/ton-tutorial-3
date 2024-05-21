ad4164c304191807a866b4ebc52b0445cf83bc3b
Lopol2010 <erizhov7@gmail.com>
2024-05-21 17:48:51 +0300

    add TWA SDK

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css'
 import { TonConnectButton } from '@tonconnect/ui-react'
 import { useCounterContract } from './hooks/useCounterContract'
 import { useTonConnect } from './hooks/useTonConnect'
+import '@twa-dev/sdk';
+
 
 function App() {
   const {counterValue, address, increment} = useCounterContract();
